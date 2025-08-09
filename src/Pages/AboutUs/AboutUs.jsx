0635b64d105c8fd4980687697045dbb6cbb50341
ashik0401 <ashik04am@gmail.com>
2025-08-09 12:05:14 +0600

    make slider more clean

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
   return (
     <section className="bg-base-200 py-16 px-6 md:px-20">
       <div className="max-w-6xl mx-auto text-center">
-        {/* Heading */}
+        
         <h2 className="text-4xl font-bold text-primary mb-4">
           About <span className="text-primary">Us</span>
         </h2>
@@ -17,9 +17,8 @@ const AboutUs = () => {
         </p>
       </div>
 
-      {/* Mission / Vision / Values */}
       <div className="grid md:grid-cols-3 gap-10 mt-14">
-        {/* Mission */}
+       
         <div className="bg-white shadow-lg rounded-xl p-8 hover:shadow-xl transition-shadow duration-300 text-center">
           <div className="flex justify-center mb-4 text-blue-600 text-4xl">
             <FaBullseye />
@@ -31,7 +30,7 @@ const AboutUs = () => {
           </p>
         </div>
 
-        {/* Vision */}
+      
         <div className="bg-white shadow-lg rounded-xl p-8 hover:shadow-xl transition-shadow duration-300 text-center">
           <div className="flex justify-center mb-4 text-green-600 text-4xl">
             <FaLightbulb />
@@ -43,7 +42,7 @@ const AboutUs = () => {
           </p>
         </div>
 
-        {/* Values */}
+       
         <div className="bg-white shadow-lg rounded-xl p-8 hover:shadow-xl transition-shadow duration-300 text-center">
           <div className="flex justify-center mb-4 text-yellow-500 text-4xl">
             <FaUsers />
@@ -56,7 +55,7 @@ const AboutUs = () => {
         </div>
       </div>
 
-      {/* Bottom Statement */}
+    
       <div className="mt-16 text-center">
         <p className="text-gray-600 max-w-2xl mx-auto">
           Together, we’re redefining the way people choose products — 
--- src/Pages/HomePages/Slider.jsx
+++ src/Pages/HomePages/Slider.jsx
@@ -4,6 +4,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router';
 
 const Slider = () => {
     const [data, setData] = useState([]);
@@ -25,7 +26,8 @@ const Slider = () => {
     return (
         <div className='lg:pb-10'>
             {loading ? (
-                <p className="text-center">Loading...</p>
+                <p className="text-center"><span class="loading loading-ring loading-xs"></span>
+                </p>
             ) : (
                 <Swiper
                     spaceBetween={30}
@@ -49,10 +51,19 @@ const Slider = () => {
                                     alt={item.title}
                                     className="w-full  max-h-[60vh] md:max-h-[65vh] lg:max-h-[70vh] object-cover "
                                 />
-                                <div className="absolute flex flex-col items-center justify-center inset-0 px-6 rounded-xl transition-all duration-500 md:pl-40 md:space-y-3">
-                                    <h2 className="md:text-3xl font-bold text-sm text-primary">{item.title}</h2>
-                                    <p className="md:text-xl md:font-medium">{item.subtitle}</p>
-                                    <div></div>
+                                <div className="absolute flex flex-col items-center justify-center inset-0 px-6 rounded-xl transition-all duration-500 md:pl-40 md:space-y-3 bg-black/30 text-white">
+                                    <h2 className="md:text-3xl font-bold text-sm ">{item.title}</h2>
+                                    <p className="md:text-xl md:font-medium text-center">{item.description}</p>
+                                    <div>
+                                        <Link to='/queries' className="relative inline-block text-lg group">
+                                            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-primary transition-colors duration-300 ease-out border-2 border-base-300 rounded-lg group-hover:text-white">
+                                                <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                                                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-base-300 group-hover:-rotate-180 ease"></span>
+                                                <span className="relative">See more</span>
+                                            </span>
+                                            <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-base-300 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </SwiperSlide>
